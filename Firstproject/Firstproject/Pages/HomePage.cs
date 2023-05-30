94ff42d7e7f2d53be78670b9b4855977084a043a
Prabavathini <prabavathini@yahoo.com>
2023-05-30 13:25:53 +1200

    Homepage added

--- Firstproject/Firstproject/Pages/HomePage.cs
+++ Firstproject/Firstproject/Pages/HomePage.cs
@@ -12,15 +12,27 @@ namespace Firstproject.Pages
     {
         public void GoToTMPage(IWebDriver driver)
         {
-            // Create new record
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+
             //Navigate time and Material page
             IWebElement administrationTab = driver.FindElement(By.XPath("/html/body/div[3]/div/div/ul/li[5]/a"));
             administrationTab.Click();
 
-            Wait.WaitToBeClickable(driver, "XPath", "/html/body/div[3]/div/div/ul/li[5]/ul/li[3]/a", 7);
+            Wait.WaitToExist(driver, "XPath", "/html/body/div[3]/div/div/ul/li[5]/ul/li[3]/a", 7);
 
-           /* IWebElement tmOption = driver.FindElement(By.XPath("/html/body/div[3]/div/div/ul/li[5]/ul/li[3]/a"));
-            tmOption.Click();*/
+            IWebElement tmOption = driver.FindElement(By.XPath("/html/body/div[3]/div/div/ul/li[5]/ul/li[3]/a"));
+            tmOption.Click();
+        }
+
+        public void GoToEmployeePage(IWebDriver driver)
+        {
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+
+            IWebElement administrationTab = driver.FindElement(By.XPath("/html/body/div[3]/div/div/ul/li[5]/a"));
+            administrationTab.Click();
+
+            Wait.WaitToExist(driver, "XPath", "/html/body/div[3]/div/div/ul/li[5]/ul/li[3]/a", 7);
+
 
             IWebElement employeeOPtion = driver.FindElement(By.XPath("/html/body/div[3]/div/div/ul/li[5]/ul/li[2]/a"));
             employeeOPtion.Click();
